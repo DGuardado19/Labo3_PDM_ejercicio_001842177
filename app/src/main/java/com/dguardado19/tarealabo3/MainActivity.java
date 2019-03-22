6222d2f10c0fdf9a2a82043e2d3a1ab83e448f8d
DGuardado19 <Piploc19>
2019-03-22 15:04:32 -0600

    Initial commit

---
+++ app/src/main/java/com/dguardado19/tarealabo3/MainActivity.java
@@ -0,0 +1,13 @@
+package com.dguardado19.tarealabo3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
